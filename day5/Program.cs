5cf9ba0807e675c0b55d316461e650e91b9bb701
Joseph Pulk <pulkjr@gmail.com>
2025-12-10 09:27:40 -0500

    Day 5

---
+++ day5/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+class IngredientChecker
+{
+    public static bool IsFresh(Dictionary<long, long> range, long itemId)
+    {
+        foreach (long start in range.Keys.AsEnumerable())
+        {
+            if (itemId == start || itemId == range[start])
+                return true;
+
+            if (itemId > start && itemId < range[start])
+                return true;
+        }
+        return false;
+    }
+
+    public static void Main()
+    {
+        string[] inputLines = File.ReadAllText("./input.txt")
+            .ReplaceLineEndings()
+            .Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+
+        var range = new Dictionary<long, long>();
+        var hasMovedIntoIngredients = false;
+        long freshItemCount = 0;
+        var stopwatch = Stopwatch.StartNew();
+        foreach (var line in inputLines)
+        {
+            Console.WriteLine(line);
+            if (!hasMovedIntoIngredients && string.IsNullOrWhiteSpace(line))
+            {
+                hasMovedIntoIngredients = true;
+                continue;
+            }
+            if (!hasMovedIntoIngredients)
+            {
+                var values = line.Split('-');
+                var start = long.Parse(values[0]);
+                var end = long.Parse(values[1]);
+                if (!range.TryAdd(start, end))
+                {
+                    if (range[start] <= end)
+                    {
+                        range[start] = end;
+                    }
+                }
+                continue;
+            }
+            if (string.IsNullOrWhiteSpace(line))
+                continue;
+            long itemId = long.Parse(line);
+            if (IsFresh(range, itemId))
+                freshItemCount++;
+        }
+        stopwatch.Stop();
+        Console.WriteLine($"Processing time : {stopwatch.ElapsedMilliseconds}ms");
+        Console.WriteLine($"There are {freshItemCount} fresh items!");
+    }
+}
